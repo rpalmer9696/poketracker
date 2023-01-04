45a14eac23bae49c65f2a6b0d693717209f19c47
Ryan Palmer <rpalmer9696@gmail.com>
2023-01-04 09:39:27 +0000

    Adds footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
   trpc.pokemon.populatePokemon.useQuery();
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div className="flex flex-auto flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           PokeTracker
@@ -17,8 +17,8 @@ const Home: NextPage = () => {
         <div className="flex flex-col items-center gap-2">
           <p className="text-2xl text-white">Ready to track some Pokemon?</p>
           {sessionData ? (
-            <Link href="/pokeList">
-              <a className="text-l text-blue-200 underline">Go to Tracker</a>
+            <Link href="/pokeList" className="text-l text-blue-200 underline">
+              Go to Tracker
             </Link>
           ) : (
             ""
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,8 +8,19 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <title>PokeTracker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="min-h-screen bg-gradient-to-b from-cyan-500 to-blue-500">
+      <main className="flex min-h-screen flex-col bg-gradient-to-b from-cyan-500 to-blue-500">
         {children}
+        <footer className="flex h-24 w-full flex-col items-center justify-center">
+          <p className="text-white">
+            Made with ❤️ by{" "}
+            <Link
+              href="https://github.com/rpalmer9696"
+              className="text-underline text-blue-200"
+            >
+              @rpalmer9696
+            </Link>
+          </p>
+        </footer>
       </main>
     </>
   );
