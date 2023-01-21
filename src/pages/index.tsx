224b50e7fc2a39337787c659202c5dc051b5f86c
Ryan Palmer <rpalmer9696@gmail.com>
2023-01-21 13:19:23 +0000

    Redirect on sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import { type NextPage } from "next";
+import { GetServerSideProps, type NextPage } from "next";
 import AuthShowcase from "@/components/authButton";
 import { trpc } from "@/utils/trpc";
-import { useSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
 import Link from "next/link";
 
 const Home: NextPage = () => {
@@ -31,3 +31,20 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: `/pokeList`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
